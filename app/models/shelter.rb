96ef50e38c43b5b68a61536cc10d8a01fae22c2f
Grayson Cunningham <gecun289@gmail.com>
2020-12-14 18:03:43 -0700

    add shelters_with_pending_apps method to Shelter class with tests

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,3 +1,7 @@
 class Shelter < ApplicationRecord
   has_many :pets
+
+  def self.shelters_with_pending_apps
+    joins(pets: [:application_pets, :applications]).where("applications.status = 'Pending'").distinct
+  end
 end
--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -4,4 +4,36 @@ describe Shelter, type: :model do
   describe 'relationships' do
     it { should have_many :pets }
   end
+
+  describe 'class methods' do
+    it 'can find shelters with pending applications' do
+      pet = create(:pet)
+      application = create(:application, pets: [pet], status: "Pending")
+      shelter_1 = pet.shelter
+      shelter_2 = create(:shelter)
+
+      expect(Shelter.shelters_with_pending_apps).to eq([shelter_1])
+    end
+
+    it '.shelters_with_pending_apps does not find "Rejected" applications' do
+      pet = create(:pet)
+      application = create(:application, pets: [pet], status: "Rejected")
+
+      expect(Shelter.shelters_with_pending_apps).to eq([])
+    end
+
+    it '.shelters_with_pending_apps does not find "Approved" applications' do
+      pet = create(:pet)
+      application = create(:application, pets: [pet], status: "Approved")
+
+      expect(Shelter.shelters_with_pending_apps).to eq([])
+    end
+
+    it '.shelters_with_pending_apps does not find "In Progress" applications' do
+      pet = create(:pet)
+      application = create(:application, pets: [pet], status: "In Progress")
+
+      expect(Shelter.shelters_with_pending_apps).to eq([])
+    end
+  end
 end
