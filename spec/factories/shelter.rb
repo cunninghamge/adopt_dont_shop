f349ff70d35e9fdb0d6692deec818ca73f65b98d
Grayson Cunningham <gecun289@gmail.com>
2020-12-16 14:44:25 -0700

    ensure unique values for FactoryBot shelter names

--- spec/factories/shelter.rb
+++ spec/factories/shelter.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :shelter do
-    name {"#{Faker::Address.community} Animal Shelter"}
+    name {"#{Faker::Address.unique.community} Animal Shelter"}
     address {Faker::Address.street_address}
     city {Faker::Address.city}
     state {Faker::Address.state_abbr}
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,13 +2,13 @@
 require 'simplecov'
 SimpleCov.start
 require 'spec_helper'
-require 'support/factory_bot.rb'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'support/factory_bot.rb'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -23,7 +23,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -69,5 +69,9 @@ RSpec.configure do |config|
       with.test_framework :rspec
       with.library :rails
     end
+  end
+
+  config.before(:each) do
+    Faker::UniqueGenerator.clear
   end
 end
